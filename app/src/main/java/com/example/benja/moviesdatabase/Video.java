ab530935f23cb4ea1c211c687520e1f3981c9c12
benjaminMunoz <benjamin.munoz@blackswan.com>
2018-09-25 14:56:08 -0700

    Feat: Add video class for trailers

---
+++ app/src/main/java/com/example/benja/moviesdatabase/Video.java
@@ -0,0 +1,76 @@
+package com.example.benja.moviesdatabase;
+
+public class Video  {
+    private String id;
+    private String iso639;
+    private String iso3166;
+    private String key;
+    private String name;
+    private String site;
+    private String size;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIso639() {
+        return iso639;
+    }
+
+    public void setIso639(String iso639) {
+        this.iso639 = iso639;
+    }
+
+    public String getIso3166() {
+        return iso3166;
+    }
+
+    public void setIso3166(String iso3166) {
+        this.iso3166 = iso3166;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
